f7f751356787105c846d70fb2c8d7fc860db1429
Thiago Carvalho <thgrcarvalho@gmail.com>
2021-03-10 10:56:06 -0300

    Creating java Spring Boot project.

---
+++ src/test/java/com/challenge/GithubCrawler/GithubCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.GithubCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
