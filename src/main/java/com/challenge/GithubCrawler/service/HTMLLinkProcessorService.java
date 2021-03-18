5372e72142243874e5d5af052ecb57646454d7dc
Thiago Carvalho <thgrcarvalho@gmail.com>
2021-03-17 23:01:15 -0300

    Adding the javadocs of a few classes.

--- src/main/java/com/challenge/GithubCrawler/service/HTMLLinkProcessorService.java
+++ src/main/java/com/challenge/GithubCrawler/service/HTMLLinkProcessorService.java
@@ -9,6 +9,14 @@ import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Service Class that is responsible for processing the links discovered by the API.
+ *
+ * @version 1.0
+ *
+ * @author Thiago Carvalho
+ *
+ */
 @Service
 public class HTMLLinkProcessorService implements LinkProcessingService {
     @Autowired
@@ -17,6 +25,15 @@ public class HTMLLinkProcessorService implements LinkProcessingService {
     @Autowired
     private PageAndFileProcessingService pageAndFileProcessor;
 
+    /**
+     * Function responsible for processing a link found by the scraping process.
+     *
+     * @param link Requested link.
+     * @return This function is responsible only for processing the URL. Nothing is returned.
+     * @throws IOException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     @Async("asyncExecutor")
     public CompletableFuture<Void> processLink(Link link) throws IOException, ExecutionException, InterruptedException {
 
@@ -32,6 +49,15 @@ public class HTMLLinkProcessorService implements LinkProcessingService {
         return null;
     }
 
+    /**
+     * Function responsible for sending the request to process all links found by the API process.
+     *
+     * @param link Requested link.
+     * @return This function is responsible only for processing the URL. Nothing is returned.
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws IOException
+     */
     private CompletableFuture<Void> sendPageOrFileRequestProcess(Link link) throws InterruptedException, ExecutionException, IOException {
         CompletableFuture<Void> isReady;
 
@@ -44,10 +70,22 @@ public class HTMLLinkProcessorService implements LinkProcessingService {
         return isReady;
     }
 
+    /**
+     * Function that verifies if a link was already visited.
+     *
+     * @param URL Requested link URL.
+     * @return The result of the verification.
+     */
     private Boolean verifyIfLinkedWasVisited(String URL){
         return this.githubRepoScraper.getVisitedLinks().contains(URL);
     }
 
+    /**
+     * Function that verifies if a link is associated to a file, instead of a folder page.
+     *
+     * @param URL Requested link URL.
+     * @return The result of the verification.
+     */
     private Boolean checkIfLinkIsFile(String URL){
         return URL.contains(this.githubRepoScraper.getGithubFileType());
     }
